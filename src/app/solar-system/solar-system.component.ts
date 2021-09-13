5f050d8673d984d3e213e5a0d78ae8e55bdb129f
barti132 <barteksredzinski@gmail.com>
2021-09-13 18:43:48 +0200

    add: sun sphere

--- src/app/solar-system/solar-system.component.ts
+++ src/app/solar-system/solar-system.component.ts
@@ -1,31 +1,43 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import * as THREE from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
-
+import Scene = THREE.Scene;
+import Mesh = THREE.Mesh;
+import Camera = THREE.Camera;
 
 @Component({
   selector: 'app-solar-system',
-  templateUrl: './solar-system.component.html',
+  templateUrl: 'solar-system.component.html',
   styleUrls: ['./solar-system.component.css']
 })
 export class SolarSystemComponent implements AfterViewInit{
   @ViewChild('rendererContainer') rendererContainer!: ElementRef;
 
   private renderer = new THREE.WebGLRenderer;
-  private scene:any = null;
-  private camera:any = null;
-  private mesh:any = null;
+  private scene:Scene;
+  private camera:Camera;
+  private mesh:Mesh;
+  private controls:any = null;
+  private textureLoader = new THREE.TextureLoader();
+  private sunSurface = new THREE.MeshStandardMaterial({color:0xaaaaaa});
 
   constructor() {
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    this.camera.position.z = 1000;
+    this.camera.position.set(0, 10, 100);
 
-    const geometry = new THREE.BoxGeometry(200, 200, 200);
-    const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-    this.mesh = new THREE.Mesh(geometry, material);
+    this.scene.add(new THREE.AmbientLight(0xFFFFFF, 1.5));
+
+    const geometry = new THREE.SphereGeometry( 15, 32, 16 );
+
+    this.sunSurface.map = this.textureLoader.load('./assets/2k_sun.jpg');
+
+    this.mesh = new THREE.Mesh(geometry, this.sunSurface);
 
     this.scene.add(this.mesh);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
   }
 
   ngAfterViewInit(): void{
@@ -36,8 +48,7 @@ export class SolarSystemComponent implements AfterViewInit{
 
   animate(): void{
     window.requestAnimationFrame(() => this.animate());
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.02;
+    this.controls.update();
     this.renderer.render(this.scene, this.camera);
   }
 
