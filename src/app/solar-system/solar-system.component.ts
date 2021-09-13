c2aea7c2f413418256b6210d6af7a4665566df80
barti132 <barteksredzinski@gmail.com>
2021-09-13 02:33:38 +0200

    add: dummy sample of threeJS

---
+++ src/app/solar-system/solar-system.component.ts
@@ -0,0 +1,44 @@
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import * as THREE from 'three';
+
+
+
+@Component({
+  selector: 'app-solar-system',
+  templateUrl: './solar-system.component.html',
+  styleUrls: ['./solar-system.component.css']
+})
+export class SolarSystemComponent implements AfterViewInit{
+  @ViewChild('rendererContainer') rendererContainer!: ElementRef;
+
+  private renderer = new THREE.WebGLRenderer;
+  private scene:any = null;
+  private camera:any = null;
+  private mesh:any = null;
+
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    this.camera.position.z = 1000;
+
+    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+    this.mesh = new THREE.Mesh(geometry, material);
+
+    this.scene.add(this.mesh);
+  }
+
+  ngAfterViewInit(): void{
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
+    this.animate();
+  }
+
+  animate(): void{
+    window.requestAnimationFrame(() => this.animate());
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.02;
+    this.renderer.render(this.scene, this.camera);
+  }
+
+}
