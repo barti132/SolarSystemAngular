4eef3e8705e42cc1d5142663ea7f4d73d94be37d
barti132 <barteksredzinski@gmail.com>
2021-09-13 02:33:05 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SolarSystemComponent } from './solar-system/solar-system.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SolarSystemComponent
   ],
   imports: [
     BrowserModule
